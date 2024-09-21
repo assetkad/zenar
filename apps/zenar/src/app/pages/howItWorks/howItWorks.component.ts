5c0449e9ba5438f0e494621c485546001d5aeb55
assetkad <asetkadyrzhanov@gmail.com>
2024-09-21 06:11:20 +0500

    layout of 5 pages done

--- apps/zenar/src/app/pages/howItWorks/howItWorks.component.ts
+++ apps/zenar/src/app/pages/howItWorks/howItWorks.component.ts
@@ -1,13 +1,36 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { FooterComponent, HeaderComponent } from '@zenar/ui-kit';
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-how-it-works',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    FooterComponent,
+    FormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+  ],
   templateUrl: './howItWorks.component.html',
   styleUrl: './howItWorks.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HowItWorksComponent {}
+export class HowItWorksComponent {
+  foods: Food[] = [
+    { value: 'steak-0', viewValue: 'Steak' },
+    { value: 'pizza-1', viewValue: 'Pizza' },
+    { value: 'tacos-2', viewValue: 'Tacos' },
+  ];
+}
